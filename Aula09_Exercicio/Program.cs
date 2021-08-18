cb9a2076fb42017b9d047afadaf1551ae923a9fe
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-08-18 20:55:19 -0300

    Add project files.

---
+++ Aula09_Exercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula09_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual a idade do primeiro aluno?");
+            int primeiro = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a idade do segundo aluno?");
+            int segundo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a idade do terceiro aluno?");
+            int terceiro = int.Parse(Console.ReadLine());
+
+            int media = (primeiro + segundo + terceiro) / 3;
+            Console.WriteLine("\nA idade média dos alunos é de " + media + " anos!");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
